9a7d97c3f52556ef8ec7feb1f87a17f32bcff935
bleepbloop13 <rbak9218@740s-pl-161.local>
2016-01-06 12:22:58 -0700

    Linked all views to new view controllers

--- AboutMe/CardGamesViewConrtroller.swift
+++ AboutMe/CardGamesViewConrtroller.swift
@@ -1,26 +1,25 @@
 //
-//  ViewController.swift
+//  CardGamesViewConrtroller.swift
 //  AboutMe
 //
 //  Created by Baker, Rory on 1/6/16.
 //  Copyright © 2016 Baker, Rory. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController
+class CardGamesViewController : UIViewController
 {
-
     override func viewDidLoad()
     {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning()
     {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 }
-
